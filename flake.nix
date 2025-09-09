95a4679fcc8ebd835ddde208bec4caeee639e982
moontoaster <74986506+moontoaster@users.noreply.github.com>
2025-09-09 21:08:17 +0300

    initial commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      inherit (nixpkgs.lib) genAttrs;
+
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      instantiateSystem =
+        f: system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        f pkgs;
+
+      forEachSystem = f: genAttrs supportedSystems (system: instantiateSystem f system);
+    in
+    {
+      devShells = forEachSystem (pkgs: {
+        default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            cargo
+            rust-analyzer
+          ];
+        };
+      });
+    };
+}
